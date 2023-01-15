7a17eb4d482e72cfcf8321bf02dfc3c42b5e65de
Keith Lea <keithl@gmail.com>
2023-01-15 14:32:46 -0500

    Initial commit! Works!

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # weather-logger
-Log current weather conditions for a specific location to Datadog
+
+Log current weather conditions for a specific location, from visualcrossing.com, to Datadog
+
+# Usage
+
+```
+export DATADOG_API_KEY=da39a3ee5e6b4b0d3255bfef95601890afd80709
+export VISUALCROSSIG_API_KEY=da39a3ee5e6b4b0d3255bfef95601890afd80709
+node index.js "New York, NY"
+```
+
+Sample log:
+
+```json
+{
+  "level": "info",
+  "message": {
+    "outsideConditions": "Clear",
+    "outsideFeelsLikeF": 35.1,
+    "outsideHumidity": 55.6,
+    "outsideSolarEnergy": 0.9,
+    "outsideSolarRadiation": 263,
+    "outsideTempF": 35.1,
+    "outsideWindMph": 18.2
+  },
+  "service": "nestlogger"
+}
+```
+
+## Datadog API key
+
+Your Datadog API key can be generated via the website ([docs](https://docs.datadoghq.com/account_management/api-app-keys/))
+
+## Visual Crossing API key
+
+Your Visual Crossing weather API Key is shown at https://www.visualcrossing.com/account.
---
+++ index.js
@@ -0,0 +1,75 @@
+const { createLogger, format, transports } = require("winston");
+const axios = require("axios");
+
+const address = process.argv[2];
+
+const datadogApiKey = process.env.DATADOG_API_KEY;
+
+if (!datadogApiKey) {
+  console.log("ERROR: $DATADOG_API_KEY is not set");
+  process.exit(1);
+}
+
+const visualCrossingApiKey = process.env.VISUAL_CROSSING_API_KEY;
+
+if (!visualCrossingApiKey) {
+  console.log("ERROR: $VISUAL_CROSSING_API_KEY is not set");
+  process.exit(1);
+}
+
+const logger = createLogger({
+  level: "debug",
+  exitOnError: false,
+  format: format.json(),
+  defaultMeta: { service: "nestlogger" },
+  transports: [
+    new transports.Http({
+      host: "http-intake.logs.datadoghq.com",
+      path: `/api/v2/logs?dd-api-key=${datadogApiKey}&ddsource=nodejs&service=nestlogger`,
+      ssl: true,
+      handleExceptions: true,
+      handleRejections: true,
+      level: "info",
+    }),
+    new transports.Console({
+      handleExceptions: true,
+      handleRejections: true,
+    }),
+  ],
+});
+
+function handleResponse(json) {
+  console.log("got response: ", json);
+  const conditions = json.currentConditions;
+  logger.info({
+    outsideTempF: conditions.temp,
+    outsideFeelsLikeF: conditions.temp,
+    outsideHumidity: conditions.humidity,
+    outsideConditions: conditions.conditions,
+    outsideWindMph: conditions.windspeed,
+    outsideSolarRadiation: conditions.solarradiation,
+    outsideSolarEnergy: conditions.solarenergy,
+  });
+}
+
+const req = {
+  method: "GET",
+  timeout: 30_000,
+  url: `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
+    address
+  )}?unitGroup=us&include=current&key=${visualCrossingApiKey}&contentType=json`,
+};
+
+axios(req)
+  .then((result) => {
+    if (result.status == 200) {
+      handleResponse(result.data);
+    } else {
+      console.error("Failed", result.status, result.statusText, result.data);
+      process.exit(1);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
